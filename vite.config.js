6743b43ccfc0b3199084be93925ad20b6eb15d09
Lakkireddygarisai <sairamsairamvivek@gmail.com>
2023-06-22 15:02:52 +0530

    Added 'portfolio_threejs' submodule

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 2000,
-  },
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000, // Adjust the limit according to your needs
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ['lodash'], // Example: manually specify lodash as a separate chunk
+        },
+      },
+    },
+  },
+});
