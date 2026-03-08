96b9f2b9e8eb1c1b7210dd4e9a5c2f9cdfb9f139
LAKKIREDDYGARI SAIRAM <Sairamsairamvivek@gmail.com>
2026-03-08 17:36:17 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,21 +3,19 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div>
-      <h2 className={`${styles.sectionHeadText} text-center`}>
-          Skills
-        </h2>
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
+        <div
+          className="w-28 h-28"
+          key={technology.name}
+          title={technology.name}   // Tooltip added here
+        >
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
-    </div>
     </div>
   );
 };
